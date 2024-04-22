f2a507ada379b867e895e398ed716f852d33ea92
Anthony <Anteemony@gmail.com>
2024-04-22 21:41:28 +0100

    update onclick

--- rag_script.py
+++ rag_script.py
@@ -1,13 +1,19 @@
 import streamlit as st
 
+
+def process_inputs():
+    if not st.session_state.unify_api_key or not st.session_state.endpoint or not st.session_state.pdf_docs:
+        st.warning("Please enter the missing fields and upload your pdf document(s)")
+
+
 def landing_page():
     st.set_page_config("Unify Demos: RAG")
 
     with st.sidebar:
-        unify_api_key = st.text_input("Unify API Key*", type="password", placeholder="Enter Unify API Key")
-        endpoint = st.text_input("Endpoint (model@provider)*", placeholder="model@provider", value="llama-2-70b-chat@anyscale")
-        pdf_docs = st.file_uploader(label="Upload PDF Document(s)*", type="pdf", accept_multiple_files=True)
-        st.button("Submit Document(s)")
+        unify_api_key = st.text_input("Unify API Key*", type="password", placeholder="Enter Unify API Key", key="unify_api_key")
+        endpoint = st.text_input("Endpoint (model@provider)*", placeholder="model@provider", value="llama-2-70b-chat@anyscale", key="endpoint")
+        pdf_docs = st.file_uploader(label="Upload PDF Document(s)*", type="pdf", accept_multiple_files=True, key="pdf_docs")
+        st.button("Submit Document(s)", on_click=process_inputs)
 
     st.title("Unify Demos: RAG Playground")
     st.text("Chat with your PDF file using the LLM of your choice")
