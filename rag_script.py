e07d2a5daac1d4aaaceea1fdbe4e5f5353b61f42
AO <90141191+Anteemony@users.noreply.github.com>
2024-04-24 03:03:59 -0700

    Update rag_script.py

--- rag_script.py
+++ rag_script.py
@@ -56,11 +56,11 @@ def landing_page():
     st.set_page_config("Unify Demos: RAG")
 
     with st.sidebar:
-        unify_api_key = st.text_input("Unify API Key*", type="password", placeholder="Enter Unify API Key", key="unify_api_key")
-        endpoint = st.text_input("Endpoint (model@provider)*", placeholder="model@provider", value="llama-2-70b-chat@anyscale", key="endpoint")
-        pdf_docs = st.file_uploader(label="Upload PDF Document(s)*", type="pdf", accept_multiple_files=True, key="pdf_docs")
-        st.button("Submit Document(s)", on_click=process_inputs)
-
+	unify_api_key = st.text_input("Unify API Key*", type="password", placeholder="Enter Unify API Key", key="unify_api_key")
+	endpoint = st.text_input("Endpoint (model@provider)*", placeholder="model@provider", value="llama-2-70b-chat@anyscale", key="endpoint")
+	pdf_docs = st.file_uploader(label="Upload PDF Document(s)*", type="pdf", accept_multiple_files=True, key="pdf_docs")
+	st.button("Submit Document(s)", on_click=process_inputs)
+	
 	st.title("Unify Demos: RAG Playground")
 	st.text("Chat with your PDF file using the LLM of your choice")
 	st.write('''
