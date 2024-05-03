7d74fb1181d1491928fa08bda41fdf76b733aa28
Anthony <Anteemony@gmail.com>
2024-05-03 08:03:42 +0100

    old chain

--- rag_script.py
+++ rag_script.py
@@ -29,13 +29,23 @@ def ask_unify(query):
     retriever = vectorstore.as_retriever()
 
     prompt_template = '''
-    Use the provided context to answer the question.
-    \nContext: {context} 
-    \nQuestion: {question} 
-    \n\n Answer:
+    Use the following context (delimited by <ctx></ctx>) and the chat history (delimited by <hs></hs>) to answer the question (delimited by <qn></qn>):
+    ------
+    <ctx>
+    {context}
+    </ctx>
+    ------
+    <hs>
+    {chat_history}
+    </hs>
+    ------
+    <qn>
+    {question}
+    </qn>
+    Answer:
     '''
     model = ChatUnify(model=st.session_state.endpoint, unify_api_key=st.session_state.unify_api_key)
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question", "chat_history"])
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "chat_history", "question"])
 
     
     qa_chain = ConversationalRetrievalChain.from_llm(
@@ -43,7 +53,9 @@ def ask_unify(query):
         retriever=retriever,
         return_source_documents=True,
         combine_docs_chain_kwargs={"prompt": prompt},
-        max_tokens_limit=4000
+        max_tokens_limit=4000,
+        rephrase_question=False,
+        verbose=True
     )
 
     response = qa_chain({"question": query, 'chat_history': st.session_state.messages}, return_only_outputs=True)
@@ -94,11 +106,11 @@ def landing_page():
     with st.sidebar:
 
         # input for Unify API Key
-        st.session_state.unify_api_key = st.text_input("Unify API Key*", type="password", on_change=field_callback, placeholder="Enter Unify API Key")
+        st.session_state.unify_api_key = st.text_input("Unify API Key*", type="password", placeholder="Enter Unify API Key")
         # Model and provider selection 
         model_provider = {"mixtral-8x7b-instruct-v0.1": ["together-ai", "octoai", "replicate", "mistral-ai", "perplexity-ai", "anyscale", "fireworks-ai", "lepton-ai", "deepinfra", "aws-bedrock"], "llama-2-70b-chat": ["anyscale", "perplexity-ai", "together-ai", "replicate", "octoai", "fireworks-ai", "lepton-ai", "deepinfra", "aws-bedrock"], "llama-2-13b-chat": ["anyscale", "together-ai", "replicate", "octoai", "fireworks-ai", "lepton-ai", "deepinfra", "aws-bedrock"], "mistral-7b-instruct-v0.2": ["perplexity-ai", "together-ai", "mistral-ai", "replicate", "aws-bedrock", "octoai", "fireworks-ai"], "llama-2-7b-chat": ["anyscale", "together-ai", "replicate", "fireworks-ai", "lepton-ai", "deepinfra"], "codellama-34b-instruct": ["anyscale", "perplexity-ai", "together-ai", "octoai", "fireworks-ai", "deepinfra"], "gemma-7b-it": ["anyscale", "together-ai", "fireworks-ai", "lepton-ai", "deepinfra"], "mistral-7b-instruct-v0.1": ["anyscale", "together-ai", "fireworks-ai", "deepinfra"], "mixtral-8x22b-instruct-v0.1": ["mistral-ai", "together-ai", "fireworks-ai", "deepinfra"], "codellama-13b-instruct": ["together-ai", "octoai", "fireworks-ai"], "codellama-7b-instruct": ["together-ai", "octoai"], "yi-34b-chat": ["together-ai", "deepinfra"], "llama-3-8b-chat": ["together-ai", "fireworks-ai"], "llama-3-70b-chat": ["together-ai", "fireworks-ai"], "pplx-7b-chat": ["perplexity-ai"], "mistral-medium": ["mistral-ai"], "gpt-4": ["openai"], "pplx-70b-chat": ["perplexity-ai"], "gpt-3.5-turbo": ["openai"], "deepseek-coder-33b-instruct": ["together-ai"], "gemma-2b-it": ["together-ai"], "gpt-4-turbo": ["openai"], "mistral-small": ["mistral-ai"], "mistral-large": ["mistral-ai"], "claude-3-haiku": ["anthropic"], "claude-3-opus": ["anthropic"], "claude-3-sonnet": ["anthropic"]}
-        model_name = st.selectbox("Select Model",options=model_provider.keys(),index=20, on_change=field_callback, placeholder= "Model", args= ("Model",))
-        provider_name = st.selectbox("Select a Provider",options=model_provider[model_name], on_change=field_callback, placeholder="Provider", args = ("Provider",))		
+        model_name = st.selectbox("Select Model",options=model_provider.keys(),index=20, placeholder= "Model", args= ("Model",))
+        provider_name = st.selectbox("Select a Provider",options=model_provider[model_name], placeholder="Provider", args = ("Provider",))
         st.session_state.endpoint = f"{model_name}@{provider_name}"
         
         #Document uploader
