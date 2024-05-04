42cf88926d6ba960614ad077d90500623431842f
Anthony <Anteemony@gmail.com>
2024-05-04 16:36:05 +0100

    Add Sidebar tabs

--- rag_script.py
+++ rag_script.py
@@ -142,64 +142,71 @@ def landing_page():
     st.set_page_config("Unify Demos: RAG")
 
     with st.sidebar:
-        # input for Unify API Key
-        st.session_state.unify_api_key = st.text_input("Unify API Key*", type="password", on_change=field_callback,
-                                                       placeholder="Enter Unify API Key", args=("Unify Key ",))
-        # Model and provider selection
-        model_provider = {
-            "mixtral-8x7b-instruct-v0.1": ["together-ai", "octoai", "replicate", "mistral-ai", "perplexity-ai",
-                                           "anyscale", "fireworks-ai", "lepton-ai", "deepinfra", "aws-bedrock"],
-            "llama-2-70b-chat": ["anyscale", "perplexity-ai", "together-ai", "replicate", "octoai", "fireworks-ai",
-                                 "lepton-ai", "deepinfra", "aws-bedrock"],
-            "llama-2-13b-chat": ["anyscale", "together-ai", "replicate", "octoai", "fireworks-ai", "lepton-ai",
-                                 "deepinfra", "aws-bedrock"],
-            "mistral-7b-instruct-v0.2": ["perplexity-ai", "together-ai", "mistral-ai", "replicate", "aws-bedrock",
-                                         "octoai", "fireworks-ai"],
-            "llama-2-7b-chat": ["anyscale", "together-ai", "replicate", "fireworks-ai", "lepton-ai", "deepinfra"],
-            "codellama-34b-instruct": ["anyscale", "perplexity-ai", "together-ai", "octoai", "fireworks-ai",
-                                       "deepinfra"],
-            "gemma-7b-it": ["anyscale", "together-ai", "fireworks-ai", "lepton-ai", "deepinfra"],
-            "mistral-7b-instruct-v0.1": ["anyscale", "together-ai", "fireworks-ai", "deepinfra"],
-            "mixtral-8x22b-instruct-v0.1": ["mistral-ai", "together-ai", "fireworks-ai", "deepinfra"],
-            "codellama-13b-instruct": ["together-ai", "octoai", "fireworks-ai"],
-            "codellama-7b-instruct": ["together-ai", "octoai"], "yi-34b-chat": ["together-ai", "deepinfra"],
-            "llama-3-8b-chat": ["together-ai", "fireworks-ai"], "llama-3-70b-chat": ["together-ai", "fireworks-ai"],
-            "pplx-7b-chat": ["perplexity-ai"], "mistral-medium": ["mistral-ai"], "gpt-4": ["openai"],
-            "pplx-70b-chat": ["perplexity-ai"], "gpt-3.5-turbo": ["openai"],
-            "deepseek-coder-33b-instruct": ["together-ai"], "gemma-2b-it": ["together-ai"], "gpt-4-turbo": ["openai"],
-            "mistral-small": ["mistral-ai"], "mistral-large": ["mistral-ai"], "claude-3-haiku": ["anthropic"],
-            "claude-3-opus": ["anthropic"], "claude-3-sonnet": ["anthropic"]}
-        dynamic_provider = ["lowest-input-cost", "lowest-output-cost", "lowest-itl", "lowest-ttft", "highest-tks-per-sec"]
-        model_name = st.selectbox("Select Model", options=model_provider.keys(), index=20, on_change=field_callback,
-                                  placeholder="Model", args=("Model",))
-        if st.toggle("Enable Dynamic Routing"):
-            provider_name = st.selectbox("Select a Provider", options=dynamic_provider,
-                                         on_change=field_callback,
-                                         placeholder="Provider", args=("Provider",))
-        else:
-            provider_name = st.selectbox("Select a Provider", options=model_provider[model_name],
-                                         on_change=field_callback,
-                                         placeholder="Provider", args=("Provider",))
-        st.session_state.endpoint = f"{model_name}@{provider_name}"
+        tab1, tab2, tab3 = st.tabs(["🏠Home", "🛝Playground", "🎉Generate"])
 
-        # Document uploader
-        st.session_state.pdf_docs = st.file_uploader(label="Upload PDF Document(s)*", type="pdf",
-                                                     accept_multiple_files=True)
-        if st.button("Submit Document(s)"):
-            process_inputs()
+        with tab1:
+            # input for Unify API Key
+            st.session_state.unify_api_key = st.text_input("Unify API Key*", type="password", on_change=field_callback,
+                                                           placeholder="Enter Unify API Key", args=("Unify Key ",))
+            # Model and provider selection
+            model_provider = {
+                "mixtral-8x7b-instruct-v0.1": ["together-ai", "octoai", "replicate", "mistral-ai", "perplexity-ai",
+                                               "anyscale", "fireworks-ai", "lepton-ai", "deepinfra", "aws-bedrock"],
+                "llama-2-70b-chat": ["anyscale", "perplexity-ai", "together-ai", "replicate", "octoai", "fireworks-ai",
+                                     "lepton-ai", "deepinfra", "aws-bedrock"],
+                "llama-2-13b-chat": ["anyscale", "together-ai", "replicate", "octoai", "fireworks-ai", "lepton-ai",
+                                     "deepinfra", "aws-bedrock"],
+                "mistral-7b-instruct-v0.2": ["perplexity-ai", "together-ai", "mistral-ai", "replicate", "aws-bedrock",
+                                             "octoai", "fireworks-ai"],
+                "llama-2-7b-chat": ["anyscale", "together-ai", "replicate", "fireworks-ai", "lepton-ai", "deepinfra"],
+                "codellama-34b-instruct": ["anyscale", "perplexity-ai", "together-ai", "octoai", "fireworks-ai",
+                                           "deepinfra"],
+                "gemma-7b-it": ["anyscale", "together-ai", "fireworks-ai", "lepton-ai", "deepinfra"],
+                "mistral-7b-instruct-v0.1": ["anyscale", "together-ai", "fireworks-ai", "deepinfra"],
+                "mixtral-8x22b-instruct-v0.1": ["mistral-ai", "together-ai", "fireworks-ai", "deepinfra"],
+                "codellama-13b-instruct": ["together-ai", "octoai", "fireworks-ai"],
+                "codellama-7b-instruct": ["together-ai", "octoai"], "yi-34b-chat": ["together-ai", "deepinfra"],
+                "llama-3-8b-chat": ["together-ai", "fireworks-ai"], "llama-3-70b-chat": ["together-ai", "fireworks-ai"],
+                "pplx-7b-chat": ["perplexity-ai"], "mistral-medium": ["mistral-ai"], "gpt-4": ["openai"],
+                "pplx-70b-chat": ["perplexity-ai"], "gpt-3.5-turbo": ["openai"],
+                "deepseek-coder-33b-instruct": ["together-ai"], "gemma-2b-it": ["together-ai"], "gpt-4-turbo": ["openai"],
+                "mistral-small": ["mistral-ai"], "mistral-large": ["mistral-ai"], "claude-3-haiku": ["anthropic"],
+                "claude-3-opus": ["anthropic"], "claude-3-sonnet": ["anthropic"]}
+            dynamic_provider = ["lowest-input-cost", "lowest-output-cost", "lowest-itl", "lowest-ttft", "highest-tks-per-sec"]
+            model_name = st.selectbox("Select Model", options=model_provider.keys(), index=20, on_change=field_callback,
+                                      placeholder="Model", args=("Model",))
+            if st.toggle("Enable Dynamic Routing"):
+                provider_name = st.selectbox("Select a Provider", options=dynamic_provider,
+                                             on_change=field_callback,
+                                             placeholder="Provider", args=("Provider",))
+            else:
+                provider_name = st.selectbox("Select a Provider", options=model_provider[model_name],
+                                             on_change=field_callback,
+                                             placeholder="Provider", args=("Provider",))
+            st.session_state.endpoint = f"{model_name}@{provider_name}"
 
-    st.title("Langchain RAG Playground 🛝")
-    st.text("Chat with your PDF file using the LLM of your choice")
-    st.write('''
-        Usage: 
-        1. Input your **Unify API Key.** If you don’t have one yet, log in to the [console](https://console.unify.ai/) to get yours.
-        2. Select the **Model** and endpoint provider of your choice from the drop down. You can find both model and provider information in the [benchmark interface](https://unify.ai/hub).
-        3. Upload your document(s) and click the Submit button
-        4. Chat Away!
-        ''')
+            # Document uploader
+            st.session_state.pdf_docs = st.file_uploader(label="Upload PDF Document(s)*", type="pdf",
+                                                         accept_multiple_files=True)
+            if st.button("Submit Document(s)"):
+                process_inputs()
+        with tab2:
+            st.write("coming soon..")
+        with tab3:
+            st.write("coming soon..")
 
 
 def chat_bot():
+    st.title("Langchain RAG Playground 🛝")
+    st.text("Chat with your PDF file using the LLM of your choice")
+    st.write('''
+                Usage: 
+                1. Input your **Unify API Key.** If you don’t have one yet, log in to the [console](https://console.unify.ai/) to get yours.
+                2. Select the **Model** and endpoint provider of your choice from the drop down. You can find both model and provider information in the [benchmark interface](https://unify.ai/hub).
+                3. Upload your document(s) and click the Submit button
+                4. Chat Away!
+                ''')
+
     if "messages" not in st.session_state:
         st.session_state.messages = []
     #
