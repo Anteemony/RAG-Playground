9afcbcb1ecc736a21cff6936c0cf2c51545fc048
Anthony <Anteemony@gmail.com>
2024-05-08 08:16:57 +0100

    Slider Reset Button Function

--- playground/tabs/play.py
+++ playground/tabs/play.py
@@ -1,6 +1,6 @@
 from playground import st
 from playground.document_processing import process_inputs
-from playground.utils import field_callback
+from playground.utils import field_callback, reset_slider_value
 
 def playground_tab():
     st.write("🚧 => Feature Coming Soon")
@@ -27,25 +27,34 @@ def playground_tab():
             st.write("**Adjust Parameters** ")
 
             with st.expander("Model"):
-                model_temperature = st.slider("temperature", min_value=0.0, max_value=1.0, step=0.1, value=st.session_state.model_temperature)
-                st.button("Reset", on_click=lambda: None, key="model_param_reset")
+                model_temperature = st.slider("temperature", key="slider_model_temperature", min_value=0.0, max_value=1.0, step=0.1, value=st.session_state.model_temperature)
+                reset_dict = {
+                    "slider_model_temperature": "model_temperature"
+                }
+                st.button("Reset", on_click=reset_slider_value, args=(reset_dict,), key="model_param_reset")
 
             with st.expander("Text Splitter"):
 
-                chunk_size = st.slider("chunk_size", min_value=200, max_value=10000, step=100, value=st.session_state.chunk_size)
+                chunk_size = st.slider("chunk_size", key="slider_chunk_size", min_value=200, max_value=10000, step=100, value=st.session_state.chunk_size)
                 max_overlap = min(chunk_size-99, 1000)
-                chunk_overlap = st.slider("Chunk Overlap", min_value=100, max_value= max_overlap, step=100, value=st.session_state.chunk_overlap)
-                
-                st.button("Reset", on_click=lambda: None, key="text_splitter_param_reset")
+                chunk_overlap = st.slider("Chunk Overlap", key="slider_chunk_overlap", min_value=100, max_value= max_overlap, step=100, value=st.session_state.chunk_overlap)
+
+                reset_dict = {
+                    "slider_chunk_size": "chunk_size",
+                    "slider_chunk_overlap": "chunk_overlap"
+                }
+                st.button("Reset", on_click=reset_slider_value, args=(reset_dict,), key="text_splitter_param_reset")
 
             with st.expander("Retirever 🚧"):
                 st.selectbox("Search Type", options=["similarity", "mmr", "similarity_score_threshold"])
                 st.slider("k")
-                st.slider("max_tokens_retrieved")
                 st.slider("score_threshold")
                 st.slider("fetch_k")
                 st.slider("lambda_mult")
                 st.text_input("filter")
+                with st.container(border=True):
+                    st.markdown("Set Max Tokens", help="The retrieved document tokens will be checked and reduced below this limit.")
+                    st.slider("max_tokens_retrieved")
                 st.button("Reset", on_click=lambda: None, key="retriever_param_reset")
 
             st.session_state.applied_config = False
