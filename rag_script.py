285d973af8ceef7214d0f9fa280303829695384c
Oscar Arroyo Vega <arroscar@gmail.com>
2024-05-20 17:20:43 +0200

    minimize comments for rag_script.py

--- rag_script.py
+++ rag_script.py
@@ -1,15 +1,8 @@
-"""
-Main script to run the RAG Playground application.
-
-This script initializes session keys and launches the landing page of the application.
-"""
-
 from playground import landing_page
 from playground.utils import init_keys
 
 if __name__ == "__main__":
-    # Initialize all session keys firstly
-    # All new session keys should be added in the init_keys() function
+    # Initialize session keys
     init_keys()
-
+    # Load the landing page
     landing_page()
