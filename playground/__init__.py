97ec7d4a07dedb06d1191a5f1fe67cf3c61035d2
Oscar Arroyo Vega <arroscar@gmail.com>
2024-05-21 11:59:18 +0200

    Add documentation to __init__.py

--- playground/__init__.py
+++ playground/__init__.py
@@ -6,8 +6,10 @@ from playground.tabs.generate import generate_code_tab
 
 
 def landing_page():
+    """Configures and displays the landing page."""
+    
     st.set_page_config("RAG Playground", page_icon="🎉")
-
+    
     st.title("Langchain RAG Playground 🛝")
     st.text("Chat with your PDF file using the LLM of your choice")
     st.write('''
@@ -18,21 +20,18 @@ def landing_page():
             4. Chat Away!
             ''')
 
-    # show messages
+    # show the chat messages
     for message in st.session_state.messages:
         st.chat_message('human').write(message[0])
         st.chat_message('assistant').write(message[1])
-
+        
     with st.sidebar:
         tab1, tab2, tab3 = st.tabs(["🏠Home", "🛝Playground", "🎉Generate Code"])
-
         with tab1:
             home_tab()
-
         with tab2:
             playground_tab()
-
         with tab3:
             generate_code_tab()
-
+            
     chat_bot()
